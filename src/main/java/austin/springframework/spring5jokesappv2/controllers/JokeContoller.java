0989bb932d65af66699471d9a50ee3a4567f6ea1
awu <austin.ykwu@gmail.com>
2023-03-07 22:17:55 +0800

    Add JokeContoller, JokeService Add Maven dependency

---
+++ src/main/java/austin/springframework/spring5jokesappv2/controllers/JokeContoller.java
@@ -0,0 +1,21 @@
+package austin.springframework.spring5jokesappv2.controllers;
+
+import austin.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeContoller {
+    private final JokeService jokeService;
+
+    public JokeContoller(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("joke", jokeService.getJoke());
+        return "index";
+    }
+}
---
+++ src/main/java/austin/springframework/spring5jokesappv2/services/JokeService.java
@@ -0,0 +1,5 @@
+package austin.springframework.spring5jokesappv2.services;
+
+public interface JokeService {
+    String getJoke();
+}
