86f6d8790008cbf077ffbbaef563a3b15268a083
awu <austin.ykwu@gmail.com>
2023-03-08 21:31:49 +0800

    Create Templates to show joke

--- src/main/java/austin/springframework/spring5jokesappv2/controllers/JokeController.java
+++ src/main/java/austin/springframework/spring5jokesappv2/controllers/JokeController.java
@@ -1,18 +1,19 @@
 package austin.springframework.spring5jokesappv2.controllers;
 
 import austin.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class JokeContoller {
+public class JokeController {
     private final JokeService jokeService;
 
-    public JokeContoller(JokeService jokeService) {
+    @Autowired
+    public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
-
     @RequestMapping({"/", ""})
     public String showJoke(Model model){
         model.addAttribute("joke", jokeService.getJoke());
--- src/main/java/austin/springframework/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/austin/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Service;
 public class JokeServiceImpl implements JokeService {
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     @Override
